ba7a2cb0cd8a413453e439438178a6c3fbfa00fe
Viorel Robu <wwjd.vio@gmail.com>
2020-09-09 09:39:45 +0300

    refactored from Translatable to method on GetAudits

--- app/Traits/GetAudits.php
+++ app/Traits/GetAudits.php
@@ -7,9 +7,21 @@ use App\Traits\Translatable;
 
 trait GetAudits
 {
-    use Translatable;
+    /*
+    | You have to define your own FK and translations that you would like in your history display.
+    | This variable has to be defined in each class using the GetAudits trait
+    | The format is the following
+    |      protected $dictionary = [
+    |          'foreign_key' => [
+    |                   'new_name' => 'translated_column_name',
+    |                   'model' => 'model_name',
+    |                  'property' => 'column_name'
+    |          ]
+    |      ];
+    */
 
-    /**
+
+     /**
      * Get the audits for a given model
      *
      * @param string $model
@@ -51,5 +63,22 @@ trait GetAudits
         }
 
         return $modelData->audits;
+    }
+
+    /**
+     * Translate the Foreign Keys ID's from the auditable table into human readable data
+     *
+     * @param string $item
+     * @return array
+     */
+    protected function translate(string $item)
+    {
+        foreach ($this->dictionary as $key => $val) {
+            if ($item == $key) {
+                return $val;
+            }
+        }
+
+        return false;
     }
 }
